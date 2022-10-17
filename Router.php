26fca7be4facf8379ca9eee6b5ae19f56bb2710a
SergioLunaDelgado <sergioluna13102001@gmail.com>
2022-10-17 10:01:07 -0500

    Correccion del router

--- Router.php
+++ Router.php
@@ -19,7 +19,7 @@ class Router
 
     public function comprobarRutas()
     {
-        
+
         // Proteger Rutas...
         session_start();
 
@@ -32,14 +32,23 @@ class Router
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
+        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+        $splitURL = explode('?', $currentUrl);
+
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
+            $fn = $this->getRoutes[$splitURL[0]] ?? null;
         } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
+            $fn = $this->postRoutes[$splitURL[0]] ?? null;
         }
 
+        // if ($method === 'GET') {
+        //     $fn = $this->getRoutes[$currentUrl] ?? null;
+        // } else {
+        //     $fn = $this->postRoutes[$currentUrl] ?? null;
+        // }
 
-        if ( $fn ) {
+
+        if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
