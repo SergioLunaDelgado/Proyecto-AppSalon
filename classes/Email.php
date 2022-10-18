aef00318334484440b9ef5466b6e6f5052be86e2
SergioLunaDelgado <sergioluna13102001@gmail.com>
2022-10-18 14:52:53 -0500

    Corrección del email

--- classes/Email.php
+++ classes/Email.php
@@ -3,6 +3,11 @@
 namespace Classes;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use SendinBlue\Client\Model\SendSmtpEmail;
+use GuzzleHttp\Client;
+use Exception;
 
 class Email
 {
@@ -41,7 +46,7 @@ class Email
         $contenido = "<html>";
         $contenido .= "<p><b>Hola " . $this->nombre . "</b></p>";
         $contenido .= "<p>Has creado tu cuenta en AppSalón, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='https://floating-scrubland-93211.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, ignora este mensaje</p>";
         $contenido .= "</html>";
 
@@ -49,10 +54,29 @@ class Email
         $mail->AltBody = 'Confirma tu cuenta';
 
         /* Enviar el mail */
-        $mail->send();
+        // $mail->send();
+
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
+        $apiInstance = new TransactionalEmailsApi(
+            new Client(),
+            $config
+        );
+        $sendSmtpEmail = new SendSmtpEmail();
+        $sendSmtpEmail['subject'] = 'Confirma tu cuenta';
+        $sendSmtpEmail['htmlContent'] = $contenido;
+        $sendSmtpEmail['sender'] = array('name' => 'AppSalón', 'email' => 'cuentas@appsalon.com');
+        $sendSmtpEmail['to'] = array(
+            array('email' => $this->email, 'name' => $this->nombre)
+        );
+        try {
+            $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+        } catch (Exception $e) {
+            echo '<script>console.log("error");</script>';
+        }
     }
 
-    public function enviarInstrucciones(){
+    public function enviarInstrucciones()
+    {
         /* Crear el objeto de email */
         $mail = new PHPMailer();
 
@@ -74,7 +98,7 @@ class Email
         $contenido = "<html>";
         $contenido .= "<p><b>Hola " . $this->nombre . "</b></p>";
         $contenido .= "<p>Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='https://floating-scrubland-93211.herokuapp.com/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
         $contenido .= "<p>Si tu no solicitaste este cambio, ignora este mensaje</p>";
         $contenido .= "</html>";
 
